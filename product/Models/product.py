e18045eef0df257ccc733316d54019d3acada091
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-08-27 15:11:14 -0300

    fix: arrumando erros no test_order_viewset

--- product/Models/product.py
+++ product/Models/product.py
@@ -3,7 +3,7 @@ from django.db import models
 from product.Models.category import Category
 
 class Product(models.Model):
-    Title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveSmallIntegerField(null=True)
     active = models.BooleanField(default=True)
