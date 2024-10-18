819b870fedd31334f52bdfd3e6e99fffcff9040e
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-10-18 13:11:40 -0300

    fix: removendo o arquivo de host para tentar subir o codigo

--- bookStore/settings.py
+++ bookStore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-k8-0$$!n=x)33!tm8ivpy#2ap7i#lf!l&(adu)wn5b6451sg_j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -147,13 +147,3 @@ REST_FRAMEWORK = {
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-DEBUG = int(os.environ.get("DEBUG", default=0))
-
-# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
-# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]"
-).split(" ")
